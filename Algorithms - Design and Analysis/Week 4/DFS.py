5010404ae3f2aa5ed92e75461b46681ebd0e28bb
Ivan Cai <caizixian@users.noreply.github.com>
2016-07-06 16:18:41 +0800

    Algorithms: Design and Analysis (Part 1), Week 4
    DFS Topological sorting

--- Algorithms - Design and Analysis/Week 4/DFS.py
+++ Algorithms - Design and Analysis/Week 4/DFS.py
@@ -6,24 +6,43 @@ my_graph = {} #mapping from int to a list of int
 # Read the graph from stdin
 
 for line in sys.stdin:
-    input_numbers = [int(x) for x in line.strip().split()]
-    vertex = input_numbers[0]
-    for entry in input_numbers[1:]:
-        if vertex in my_graph:
-            my_graph[vertex].append(entry)
-        else:
-            my_graph[vertex]=[entry]
+    tokens = line.strip().split()
+    if tokens[1] == "S": # Sink vertex
+        my_graph[int(tokens[0])] = None
+    else:
+        input_numbers = [int(x) for x in tokens]
+        vertex = input_numbers[0]
+        for entry in input_numbers[1:]:
+            if vertex in my_graph:
+                my_graph[vertex].append(entry)
+            else:
+                my_graph[vertex]=[entry]
 
-visited = set()
-result = []
+def topo_sort(topo_graph):
+    visited = set()
+    f_value={}
+    current_label = len(topo_graph)
+    result = []
 
-def dfs(graph, entrypoint):
-    visited.add(entrypoint)
-    result.append(entrypoint)
-    for vertex in my_graph[entrypoint]:
-        if vertex not in visited:
-            dfs(graph, vertex)
+    def dfs(graph, entrypoint):
+        nonlocal current_label
+        visited.add(entrypoint)
+        result.append(entrypoint)
+        if graph.get(entrypoint, None) is None:
+            f_value[entrypoint]=current_label
+            current_label-=1
+            return
+        for vertex in graph[entrypoint]:
+            if vertex not in visited:
+                dfs(graph, vertex)
+        f_value[entrypoint]=current_label
+        current_label-=1
+        return
 
-dfs(my_graph,1)
-
-print(result)
+    for entry in topo_graph.keys():
+        if entry not in visited:
+            dfs(topo_graph,entry)
+    return f_value
+    
+print(my_graph)
+print(topo_sort(my_graph))
