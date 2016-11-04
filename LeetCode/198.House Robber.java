da0ec7ed067f7e8f2898da7cd7bd8d0cf58b0f6f
Ivan Cai <caizixian@users.noreply.github.com>
2016-11-04 22:37:01 +0800

    LeetCode 198 Alternative

--- LeetCode/198.House Robber.java
+++ LeetCode/198.House Robber.java
@@ -3,28 +3,16 @@ public class Solution {
     int[][] m;
 
     public int rob(int[] nums) {
-        if (nums.length==0){
+        if (nums.length == 0) {
             return 0;
         }
         int size = nums.length;
-        this.m = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == j) {
-                    this.m[i][j] = nums[i];
-                } else {
-                    this.m[i][j] = Integer.MIN_VALUE;
-                }
-            }
+        this.m = new int[size][2];
+        m[0][1] = nums[0];
+        for (int i = 1; i < size; i++) {
+            m[i][0] = Math.max(m[i-1][0], m[i-1][1]);
+            m[i][1] = nums[i]+ m[i-1][0];
         }
-        for (int i = 0; i < size-1; i++) {
-            m[i][i + 1] = Math.max(nums[i],nums[i+1]);
-        }
-        for (int interval = 2; interval < size; interval++) {
-            for (int i = 0; i < size-interval; i++) {
-                m[i][i + interval] = Math.max(nums[i + interval] + m[i][i + interval - 2], m[i][i + interval - 1]);
-            }
-        }
-        return m[0][size-1];
+        return Math.max(m[size - 1][0], m[size - 1][1]);
     }
 }
