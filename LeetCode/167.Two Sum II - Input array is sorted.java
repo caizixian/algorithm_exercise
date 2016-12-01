da61d75a30a0836a637943d305c6535b0086bfd9
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-01 19:50:32 +0800

    LeetCode 167 Alternative

--- LeetCode/167.Two Sum II - Input array is sorted.java
+++ LeetCode/167.Two Sum II - Input array is sorted.java
@@ -1,15 +1,17 @@
-import java.util.Arrays;
-
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int length = numbers.length;
-        for (int index1 = 0; index1 < length - 1; index1++) {
-            int index2 = Arrays.binarySearch(numbers,
-                    index1 + 1,
-                    length,
-                    target - numbers[index1]);
-            if (index2 < length && index2 > index1) {
-                return new int[]{index1 + 1, index2 + 1};// answers are not zero based
+        int ptr1 = 0;
+        int ptr2 = length - 1;
+        while (ptr1 != ptr2) {
+            if (numbers[ptr1] + numbers[ptr2] == target) {
+                return new int[]{ptr1 + 1, ptr2 + 1};
+            } else {
+                if (target - numbers[ptr1] < numbers[ptr2]) {
+                    ptr2--;
+                } else {
+                    ptr1++;
+                }
             }
         }
         return new int[]{-1, -1};
