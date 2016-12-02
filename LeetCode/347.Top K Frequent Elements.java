12618c0f2eafd00bb99748c539e4fcdddfd9a8a3
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-02 20:24:55 +0800

    LeetCode 347 Alternative

--- LeetCode/347.Top K Frequent Elements.java
+++ LeetCode/347.Top K Frequent Elements.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.LinkedList;
 
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
@@ -9,11 +8,30 @@ public class Solution {
         for (int i : nums) {
             count.merge(i, 1, (v, one) -> v + one);
         }
-        HashMap.Entry<Integer, Integer>[] counts = count.entrySet().toArray(new HashMap.Entry[count.size()]);
-        Arrays.sort(counts, (a1, a2) -> a2.getValue().compareTo(a1.getValue()));
+        int max_count = 0;
+        for (int i : count.keySet()) {
+            int v = count.get(i);
+            if (v > max_count) {
+                max_count = v;
+            }
+        }
+        LinkedList<Integer>[] num_same_count = new LinkedList[max_count + 1];
+        for (int i = 1; i <= max_count; i++) {
+            num_same_count[i] = new LinkedList<>();
+        }
+        for (HashMap.Entry<Integer, Integer> e : count.entrySet()) {
+            num_same_count[e.getValue()].add(e.getKey());
+        }
         LinkedList<Integer> result = new LinkedList<>();
-        for (int i = 0; i < k; i++) {
-            result.add(counts[i].getKey());
+        int c = 0;
+        for (int i = max_count; i > 0; i--) {
+            if (num_same_count[i].size() > 0) {
+                result.addAll(num_same_count[i]);
+                c += num_same_count[i].size();
+                if (c == k) {
+                    break;
+                }
+            }
         }
         return result;
     }
