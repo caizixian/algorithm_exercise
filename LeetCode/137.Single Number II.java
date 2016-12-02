2f5077a665685145794415e92171e1d9864f0849
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-02 22:32:55 +0800

    LeetCode 137 Alternative

--- LeetCode/137.Single Number II.java
+++ LeetCode/137.Single Number II.java
@@ -1,10 +1,17 @@
 public class Solution {
     public int singleNumber(int[] nums) {
         int ones = 0, twos = 0;
-        for(int i = 0; i < nums.length; i++){
-            ones = (ones ^ nums[i]) & ~twos; // XOR of numbers appear once
-            twos = (twos ^ nums[i]) & ~ones; // XOR of numbers appear twice
-            // Third time a number appears, it will be removed from ones and twos
+        for (int i = 0; i < nums.length; i++) {
+            // Use bits of ones and twos as a 3 counter
+            // (twos, ones)
+            // Statuses: (0 0) (0 1) (1 0) (1 1)
+            twos |= (ones & nums[i]); // need to carry
+            ones = ones ^ nums[i]; // incr
+
+            // Reset if necessary
+            int reset = twos & ones; // if reset = 1 then 1 1
+            twos = twos ^ reset;// 1^1==0
+            ones = ones ^ reset;// 1^1==0
         }
         return ones;
     }
