3bafaa20e3f59e07d6f19da2e5b32c3d234b0251
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-09 13:53:34 +0800

    LeetCode  377

---
+++ LeetCode/377.Combination Sum IV.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        for (int n : nums) {
+            if (n <= target) {
+                dp[n] = 1;
+            }
+        }
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) {
+                if (i - n >= 0) {
+                    dp[i] += dp[i - n];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
