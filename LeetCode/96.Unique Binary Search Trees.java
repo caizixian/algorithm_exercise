9ef51f0ff0d56e5cf170cd7c2321873a7bba6181
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-19 14:21:37 +0800

    LeetCode 96 Explanation

--- LeetCode/96.Unique Binary Search Trees.java
+++ LeetCode/96.Unique Binary Search Trees.java
@@ -1,12 +1,16 @@
 public class Solution {
     public int numTrees(int n) {
-        int[] count = new int[n + 1];
+        int[] count = new int[n + 1]; // count[i] = number of trees with i nodes
         count[0] = 1;
         count[1] = 1;
         for (int i = 2; i <= n; i++) {
-            for (int j = 0; j <= i - 1; j++) {
-                // 0~i-1 (inclusive) as root.val
-                count[i] = count[i] + count[j] * count[i - j - 1];
+            for (int j = 1; j <= i; j++) {
+                // root.val = j
+                // all numbers in root.left < j
+                // (1~j exclusive) (count j-1)
+                // all numbers in root.right > j
+                // (j+1 ~ i+1 exclusive) (count i-j)
+                count[i] = count[i] + count[j - 1] * count[i - j];
             }
         }
         return count[n];
