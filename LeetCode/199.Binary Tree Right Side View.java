eb797e84acab0cd5440020a753ab37c1b30a7baa
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-20 15:05:20 +0800

    LeetCode 199 Alternative

--- LeetCode/199.Binary Tree Right Side View.java
+++ LeetCode/199.Binary Tree Right Side View.java
@@ -1,44 +1,28 @@
-import java.util.ArrayDeque;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Solution {
-    class Level {
-        TreeNode tn;
-        Integer level;
+    private List<Integer> result;
 
-        Level(TreeNode tn, Integer level) {
-            this.tn = tn;
-            this.level = level;
+    private void traverse(TreeNode root, int level) {
+        if (root == null) {
+            return;
         }
+
+        if (level == result.size()) {
+            result.add(root.val);
+        }
+
+        traverse(root.right, level + 1);
+        traverse(root.left, level + 1);
     }
 
     public List<Integer> rightSideView(TreeNode root) {
-        List<List<Integer>> levelOrderTraverse = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
+        result = new LinkedList<>();
         if (root == null) {
             return result;
         }
-        ArrayDeque<Level> to_visit = new ArrayDeque<>();
-
-        to_visit.add(new Solution().new Level(root, 0));
-        while (!to_visit.isEmpty()) {
-            Level lv = to_visit.poll();
-            if (levelOrderTraverse.size() < lv.level + 1) {
-                levelOrderTraverse.add(new ArrayList<>());
-            }
-            levelOrderTraverse.get(lv.level).add(lv.tn.val);
-            if (lv.tn.left != null) {
-                to_visit.add(new Solution().new Level(lv.tn.left, lv.level + 1));
-            }
-            if (lv.tn.right != null) {
-                to_visit.add(new Solution().new Level(lv.tn.right, lv.level + 1));
-            }
-        }
-
-        for (List<Integer> l : levelOrderTraverse) {
-            result.add(l.get(l.size() - 1));
-        }
+        traverse(root, 0);
         return result;
     }
 }
