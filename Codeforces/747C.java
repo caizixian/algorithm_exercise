8588fcaaa758e10d82c4b92c94d83ee6415d9381
Ivan Cai <caizixian@users.noreply.github.com>
2016-12-25 14:04:46 +0800

    Codeforces 747C Alternative

--- Codeforces/747C.java
+++ Codeforces/747C.java
@@ -1,30 +1,102 @@
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    class LinkedListC {
+        ListNode root;
+        ListNode end;
+        int size = 0;
+
+        void add(int val) {
+            ListNode toInsert = new ListNode(val);
+            if (size == 0) {
+                this.root = toInsert;
+                this.end = this.root;
+            } else {
+                this.end.next = toInsert;
+                this.end = this.end.next;
+            }
+            size++;
+        }
+
+        int poll() {
+            int val = root.val;
+            this.root = this.root.next;
+            size--;
+            return val;
+        }
+
+        int size() {
+            return this.size;
+        }
+
+        void merge(LinkedListC other) {
+            ListNode head, tail;
+            head = new ListNode(Integer.MIN_VALUE);
+            tail = head;
+            ListNode l1 = root;
+            ListNode l2 = other.root;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    tail.next = l1;
+                    tail = tail.next;
+                    l1 = l1.next;
+                } else {
+                    tail.next = l2;
+                    tail = tail.next;
+                    l2 = l2.next;
+                }
+            }
+            if (l1 != null) {
+                tail.next = l1;
+            } else {
+                tail.next = l2;
+            }
+            this.root = head.next;
+            this.size += other.size;
+        }
+
+        public String toString() {
+            ListNode root = this.root;
+            StringBuilder sb = new StringBuilder();
+            while (root != null) {
+                sb.append(root.val);
+                sb.append(", ");
+                root = root.next;
+            }
+            return sb.toString();
+        }
+    }
+
     class Event {
         int startTime;
-        LinkedList<Integer> content;
+        LinkedListC content;
 
-        Event(int startTime, LinkedList<Integer> content) {
+        Event(int startTime, LinkedListC content) {
             this.startTime = startTime;
             this.content = content;
         }
 
         void simulate() {
-            while (content.size() > 0) {
-                server.add(content.poll());
-            }
+            server.merge(content);
         }
     }
 
-    private PriorityQueue<Integer> server;
+    private LinkedListC server;
     private PriorityQueue<Event> events;
 
     Solution(int server) {
-        this.server = new PriorityQueue<>();
+        this.server = new LinkedListC();
         this.events = new PriorityQueue<>(Comparator.comparingInt(a -> a.startTime));
         for (int i = 1; i <= server; i++) {
             this.server.add(i);
@@ -37,7 +109,7 @@ public class Solution {
         }
         if (serverCount <= server.size()) {
             int idSum = 0;
-            LinkedList<Integer> serverUsed = new LinkedList<>();
+            LinkedListC serverUsed = new LinkedListC();
             for (int i = 0; i < serverCount; i++) {
                 int server = this.server.poll();
                 idSum += server;
