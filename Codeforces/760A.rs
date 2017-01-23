3932163693cb9ed80b8a75defa9fe553273e2460
Ivan Cai <caizixian@users.noreply.github.com>
2017-01-23 10:42:02 +0800

    Codeforces 760A

---
+++ Codeforces/760A.rs
@@ -0,0 +1,46 @@
+macro_rules! read_line {
+    ($v:ident) => {
+        let mut temp = String::new();
+        std::io::stdin().read_line(&mut temp).unwrap();
+        let $v = temp;
+    };
+    (var, $t:ty, $($v:ident), *) => {
+        read_line!(input_line);
+        let mut iter = parse_token!($t, input_line);
+        $(
+            let $v = iter.next().unwrap();
+        )*
+    };
+    (vec, $t:ty, $v:ident) => {
+        read_line!(input_line);
+        let iter = parse_token!($t, input_line);
+        let $v: Vec<$t> = iter.collect();
+    };
+    ($($v:ident; $t:ty), *) => {
+        read_line!(input_line);
+        let mut iter = input_line.split_whitespace();
+        $(
+            let $v: $t = iter.next().unwrap().parse().unwrap();
+        )*
+    };
+}
+
+macro_rules! parse_token {
+    ($t:ty, $e:expr) => {
+        $e.split_whitespace().map(|x| x.parse::<$t>().unwrap());
+    };
+}
+
+fn main() {
+    read_line!(month; usize, start_weekday; usize);
+    let days = match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => 28, // Assume that the year is non-leap.
+        _ => 0
+    };
+    let days_first_column = 7 - start_weekday + 1;
+    let days_remaining = days - days_first_column;
+    let columns = (days_remaining as f64 / 7.0).ceil() as usize + 1;
+    println!("{}", columns);
+}
